ca8d4b1f5115d221b0665856a2eae6d692f942e7
ndnestor <trabajo.nestor.098@gmail.com>
2025-02-10 14:07:21 +0100

    Optimization and class name changes in Legal Notice

--- resources/views/legalidades/aviso-legal.blade.php
+++ resources/views/legalidades/aviso-legal.blade.php
@@ -1,25 +1,27 @@
-@extends('layouts.template')
-
-@section('name-page')
-    Aviso Legal
-@endsection
-
-@section('content-page')
-    <main class="main-aviso-legal">
-        <div class="aviso-legal">
-            <div class="contenido-legal">
-                <h2>AVISO LEGAL</h2>
-                <ol>
-                    <li><strong>1. </strong>El titular de este sitio web es NESTOR SALOM, con domicilio en Montemarano, AV. IT.</li>
-                    <li><strong>2. </strong>El acceso al site de NESTOR SALOM es gratuito. Todo el contenido es propiedad intelectual de NESTOR SALOM. Cualquier utilización de los mismos contraria a las normas en materia de propiedad intelectual será perseguida con arreglo a la legislación vigente.</li>
-                    <li><strong>3. </strong>Cualquier compra dentro del site no es valida.</li>
+<x-app>
+    <x-slot name="title">Aviso Legal</x-slot>
+    
+    <main class="legal-notice">
+        <article class="legal-content">
+            <section class="legal-text">
+                <h2>Aviso Legal</h2>
+                <ol class="legal-list">
+                    <li>
+                        <strong>1. </strong>El titular de este sitio web es NESTOR SALOM, con domicilio en Montemarano, AV. IT.
+                    </li>
+                    <li>
+                        <strong>2. </strong>El acceso al sitio web de NESTOR SALOM es gratuito. Todo el contenido es propiedad intelectual de NESTOR SALOM. Cualquier utilización de los mismos contraria a las normas en materia de propiedad intelectual será perseguida conforme a la legislación vigente.
+                    </li>
+                    <li>
+                        <strong>3. </strong>Cualquier compra dentro del sitio web no es válida.
+                    </li>
                 </ol>
-            </div>
-        </div>
+            </section>
+        </article>
     </main>
-@endsection
+    
 
-
-@section('files-js')
-    <script src="../js/style.js"></script>
-@endsection
+    <x-slot name="script">
+        <script src="/js/style.js"></script>
+    </x-slot>
+</x-app>
